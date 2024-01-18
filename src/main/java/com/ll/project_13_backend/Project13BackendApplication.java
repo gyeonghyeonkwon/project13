913742c922e7d5693571da1b1c887e704a61eade
kim sangmin <sign0329@gmail.com>
2024-01-18 15:17:59 +0900

    first

---
+++ src/main/java/com/ll/project_13_backend/Project13BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ll.project_13_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project13BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project13BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/project_13_backend/Project13BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.project_13_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project13BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
